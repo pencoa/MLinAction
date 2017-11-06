31ec8cd4be44238c1b10b3ae19708bc4321d2271
randy pen <pencoa42@gmail.com>
2017-11-06 12:34:18 +0800

    svm test digits

--- SVM/test.py
+++ SVM/test.py
@@ -1,5 +1,8 @@
 from imp import reload
 import svmMLiA
 
-dataArr, labelArr = svmMLiA.loadDataSet('./testSet.txt')
-b, alphas = svmMLiA.smoP(dataArr, labelArr, 0.6, 0.001, 40)
+# dataArr, labelArr = svmMLiA.loadDataSet('./testSet.txt')
+# b, alphas = svmMLiA.smoP(dataArr, labelArr, 0.6, 0.001, ('lin', 1))
+#
+# svmMLiA.testRbf(k1=0.2)
+svmMLiA.testDigits(('rbf', 20))
