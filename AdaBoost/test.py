ab61f3ea9bdea4441302e3c79016382cbb8033f9
randy pen <pencoa42@gmail.com>
2017-11-07 11:16:38 +0800

    ada train

--- AdaBoost/test.py
+++ AdaBoost/test.py
@@ -3,5 +3,6 @@ from imp import reload
 from numpy import *
 
 dataMat, classLabels = adaboost.loadSimData()
-D = mat(ones((5, 1))/5)
-adaboost.buildStump(dataMat, classLabels, D)
+# D = mat(ones((5, 1))/5)
+# adaboost.buildStump(dataMat, classLabels, D)
+classifierArray = adaboost.adaboostTrainDS(dataMat, classLabels, 9)
