4e5329b428b31d9fe584eb6cb464c3d98f3062ca
randy pen <pencoa42@gmail.com>
2017-11-08 22:14:29 +0800

    plot roc

--- AdaBoost/test.py
+++ AdaBoost/test.py
@@ -9,9 +9,10 @@ from numpy import *
 # aa = adaboost.adaClassify([[5, 5], [0, 0]], classifierArray)
 
 dataArr, labelArr = adaboost.loadDataSet('./horseColicTraining2.txt')
-classifierArray = adaboost.adaBoostTrainDS(dataArr, labelArr, 10)
+classifierArray, aggClassEst = adaboost.adaBoostTrainDS(dataArr, labelArr, 50)
+adaboost.plotROC(aggClassEst.T, labelArr)
 
-testArr, testLabelArr = adaboost.loadDataSet('./horseColicTest2.txt')
-prediction10 = adaboost.adaClassify(testArr, classifierArray)
-errArr = mat(ones((67, 1)))
-errArr[prediction10 != mat(testLabelArr).T].sum()
+# testArr, testLabelArr = adaboost.loadDataSet('./horseColicTest2.txt')
+# prediction10 = adaboost.adaClassify(testArr, classifierArray)
+# errArr = mat(ones((67, 1)))
+# errArr[prediction10 != mat(testLabelArr).T].sum()
