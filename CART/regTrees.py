cd143c3e02e6096b87a1d5366fc2b8c6a64977f9
randy pen <pencoa42@gmail.com>
2017-11-11 17:32:37 +0800

    regTrees init

---
+++ CART/regTrees.py
@@ -0,0 +1,26 @@
+from numpy import *
+
+def loadDataSet(fileName):
+    dataMat = []
+    fr = open(fileName)
+    for line in fr.readlines():
+        curLine = line.strip().split('\t')
+        fltLine = map(float, curLine)
+        dataMat.append(fltLine)
+    return dataMat
+
+def binSplitDataSet(dataSet, feature, value):
+    mat0 = dataSet[nonzero(dataSet[:,feature] > value)[0],:][0]
+    mat1 = dataSet[nonzero(dataSet[:,feature] <= value)[0],:][0]
+    return mat0, mat1
+
+def createTree(dataSet, leafType=regLeaf, errType=regErr, ops=(1,4)):
+    feat, val = chooseBestSplit(dataSet, leafType, errType, ops)
+    if feat == None: return val
+    retTree = {}
+    retTree['spInd'] = feat
+    retTree['spVal'] = val
+    lSet, rSet = binSplitDataSet(dataSet, feat, val)
+    retTree['left'] = createTree(lSet, leafType, errType, ops)
+    retTree['right'] = createTree(rSet, leafType, errType, ops)
+    return retTree
