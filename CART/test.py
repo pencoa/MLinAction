1b091b37814a1a6e0087bb710f745aa3b5058750
randy pen <pencoa42@gmail.com>
2017-11-12 20:23:10 +0800

    tree forecast

--- CART/test.py
+++ CART/test.py
@@ -16,6 +16,16 @@ from numpy import *
 # myDatTest = regTrees.loadDataSet('./ex2test.txt')
 # myMat2Test = mat(myDatTest)
 # myTreeTest = regTrees.prune(myTree, myMat2Test)
+#
+# myMat2 = mat(regTrees.loadDataSet('./exp2.txt'))
+# regTrees.createTree(myMat2, regTrees.modelLeaf, regTrees.modelErr, (1, 10))
 
-myMat2 = mat(regTrees.loadDataSet('./exp2.txt'))
-regTrees.createTree(myMat2, regTrees.modelLeaf, regTrees.modelErr, (1, 10))
+trainMat = mat(regTrees.loadDataSet('./bikeSpeedVsIq_train.txt'))
+testMat = mat(regTrees.loadDataSet('./bikeSpeedVsIq_test.txt'))
+myTree = regTrees.createTree(trainMat, ops=(1,20))
+yHat = regTrees.createForeCast(myTree, testMat[:,0])
+corrcoef(yHat, testMat[:,1], rowvar=0)[0,1]
+
+myTree = regTrees.createTree(trainMat, regTrees.modelLeaf, regTrees.modelErr, (1,20))
+yHat = regTrees.createForeCast(myTree, testMat[:,0], regTrees.modelTreeEval)
+corrcoef(yHat, testMat[:,1], rowvar=0)[0,1]
