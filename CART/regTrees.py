aaa72705b7a6fb50c0a9d0e6b147f1a95c36736f
randy pen <pencoa42@gmail.com>
2017-11-12 16:09:24 +0800

    linearSolve

--- CART/regTrees.py
+++ CART/regTrees.py
@@ -79,3 +79,23 @@ def prune(tree, testData):
             return treeMean
         else: return tree
     else: return tree
+
+def linearSolve(dataSet):
+    m, n = shape(dataSet)
+    X = mat(ones((m, n))); Y = mat(ones((m, 1)))
+    X[:,1:n] = dataSet[:,0:n-1]; Y = dataSet[:,-1]
+    xTx = X.T*X
+    if linalg.det(xTx) == 0.0:
+        raise NameError('This matrix is singular, cannot do inverse,\n\
+        try increasing the second value of ops')
+    ws = xTx.I * (X.T * Y)
+    return ws, X, Y
+
+def modelLeaf(dataSet):
+    ws, X, Y = linearSolve(dataSet)
+    return ws
+
+def modelErr(dataSet):
+    ws, X, Y = linearSolve(dataSet)
+    yHat = X * ws
+    return sum(power(Y - yHat, 2))
--- CART/test.py
+++ CART/test.py
@@ -10,9 +10,12 @@ from numpy import *
 # myMat1 = mat(myDat1)
 # regTrees.createTree(myMat1)
 
-myDat2 = regTrees.loadDataSet('./ex2.txt')
-myMat2 = mat(myDat2)
-myTree = regTrees.createTree(myMat2, ops=(0, 1))
-myDatTest = regTrees.loadDataSet('./ex2test.txt')
-myMat2Test = mat(myDatTest)
-regTrees.prune(myTree, myMat2Test)
+# myDat2 = regTrees.loadDataSet('./ex2.txt')
+# myMat2 = mat(myDat2)
+# myTree = regTrees.createTree(myMat2, ops=(0, 1))
+# myDatTest = regTrees.loadDataSet('./ex2test.txt')
+# myMat2Test = mat(myDatTest)
+# myTreeTest = regTrees.prune(myTree, myMat2Test)
+
+myMat2 = mat(regTrees.loadDataSet('./exp2.txt'))
+regTrees.createTree(myMat2, regTrees.modelLeaf, regTrees.modelErr, (1, 10))
