a6e7cb113a1c216a3be65a9f0e89d3966c5fe2a6
randy pen <pencoa42@gmail.com>
2017-11-12 22:15:39 +0800

    tkinter gui

--- CART/treeExplore.py
+++ CART/treeExplore.py
@@ -3,15 +3,52 @@ from numpy import *
 from tkinter import *
 import regTrees
 
+import matplotlib
+matplotlib.use('TkAgg')
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+
 def reDraw(tolS, tolN):
-    pass
+    reDraw.f.clf()
+    reDraw.a = reDraw.f.add_subplot(111)
+    if chkBtnVar.get():
+        if tolN < 2: tolN = 2
+        myTree = regTrees.createTree(reDraw.rawDat, regTrees.modelLeaf,\
+                                    regTrees.modelErr, (tolS, tolN))
+        yHat = regTrees.createForeCast(myTree, reDraw.testDat, \
+                                        regTrees.modelTreeEval)
+    else:
+        myTree = regTrees.createTree(reDraw.rawDat, ops=(tolS, tolN))
+        yHat = regTrees.createForeCast(myTree, reDraw.testDat)
+    reDraw.a.scatter([reDraw.rawDat[:,0]], [reDraw.rawDat[:,1]], s=5)
+    reDraw.a.plot(reDraw.testDat, yHat, linewidth=2.0)
+    reDraw.canvas.show()
+
+def getInputs():
+    try: tolN = int(tolNentry.get())
+    except:
+        tolN = 10
+        print("enter Integer for tolN")
+        tolNentry.delete(0, END)
+        tolNentry.insert(0, '10')
+    try: tolS = float(tolSentry.get())
+    except:
+        tolS = 1.0
+        print("enter Float for tolS")
+        tolSentry.delete(0, END)
+        tolSentry.insert(0, '1.0')
+    return tolN, tolS
 
 def drawNewTree():
-    pass
+    tolN, tolS = getInputs()
+    reDraw(tolS, tolN)
 
 root = Tk()
 
-Label(root, text="Plot Place Holder").grid(row=0, columnspan=3)
+reDraw.f = Figure(figsize=(5,4), dpi=100) #create canvas
+reDraw.canvas = FigureCanvasTkAgg(reDraw.f, master=root)
+reDraw.canvas.show()
+reDraw.canvas.get_tk_widget().grid(row=0, columnspan=3)
 
 Label(root, text="tolN").grid(row=1, column=0)
 tolNentry = Entry(root)
