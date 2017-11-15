db1e91f51c535283314457c687f360e7fe6b3b0a
randy pen <pencoa42@gmail.com>
2017-11-15 16:35:04 +0800

    bi-kMeans

--- K-Means/test.py
+++ K-Means/test.py
@@ -2,7 +2,10 @@ import kMeans
 from numpy import *
 from imp import reload
 
-dataMat = mat(kMeans.loadDataSet('./testSet.txt'))
-kMeans.randCent(dataMat, 2)
-kMeans.distEclud(dataMat[0], dataMat[1])
-myCentroids, clustAssing = kMeans.kMeans(dataMat, 4)
+# dataMat = mat(kMeans.loadDataSet('./testSet.txt'))
+# kMeans.randCent(dataMat, 2)
+# kMeans.distEclud(dataMat[0], dataMat[1])
+# myCentroids, clustAssing = kMeans.kMeans(dataMat, 4)
+
+datMat3 = mat(kMeans.loadDataSet('./testSet2.txt'))
+centList, myNewAssments = kMeans.biKmeans(datMat3, 3)
